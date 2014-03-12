48519704a5c7a2022e57dd1863d609418ae510e9
Sean Biefeld <sbiefeld@gmail.com>
2014-03-12 10:19:52 -0500

    beginning fizzbuzz

---
+++ HeadspringFizzBuzzLCE/HeadspringFizzBuzzLCE/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadspringFizzBuzzLCE
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			for (int i = 1; i <= 100; i++)
+			{
+				var fizz = i%3 == 0 ? "fizz" : "";
+				var buzz = i%5 == 0 ? "buzz" : "";
+				var fizzbuzz = i%3 == 0 && i%5 == 0 ? "fizzbuzz" : "";
+
+				if (!String.IsNullOrEmpty(fizzbuzz))
+				{
+					Console.WriteLine(fizzbuzz);
+				}
+				else if (!String.IsNullOrEmpty(fizz))
+				{
+					Console.WriteLine(fizz);
+
+				} else if (!String.IsNullOrEmpty(buzz))
+				{
+					Console.WriteLine(buzz);
+
+				} 
+				else
+				{
+					Console.WriteLine(i);
+				}
+			}
+			Console.ReadLine();
+		}
+	}
+}
