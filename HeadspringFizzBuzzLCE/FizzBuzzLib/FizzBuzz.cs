118dc48f9407b984b4426166c4ed61f60899f7bc
Sean Biefeld <sbiefeld@gmail.com>
2014-03-12 21:45:29 -0500

    added comments

--- HeadspringFizzBuzzLCE/FizzBuzzLib/FizzBuzz.cs
+++ HeadspringFizzBuzzLCE/FizzBuzzLib/FizzBuzz.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace FizzBuzzLib
 {
 	public class FizzBuzz
 	{
+		/// <summary>
+		/// Default fizzbuzz example, with begin/end params
+		/// </summary>
+		/// <param name="begin">starts at this number</param>
+		/// <param name="end">ends at this number</param>
+		/// <returns></returns>
 		public IEnumerable<string> DoTheFizzBuzz(int begin, int end)
 		{
 			for (int i = begin; i <= end; i++)
@@ -35,16 +40,25 @@ namespace FizzBuzzLib
 			}
 		}
 
+		/// <summary>
+		/// Using dictionary to capture custom combinations of intergers and text values
+		/// </summary>
+		/// <param name="combos">integer - text combinations to return</param>
+		/// <param name="begin">start at this number</param>
+		/// <param name="end">end at this number</param>
+		/// <returns></returns>
 		public IEnumerable<string> DoCustomFizzBuzz(IDictionary<int, string> combos, int begin, int end)
 		{
 			for (int i = begin; i <= end; i++)
 			{
 				string value = null;
 
+				//check for numbers specified in combos dictionary
 				foreach (int number in combos.Keys)
 				{
 					if (i % number == 0)
 					{
+						//append so that number that have many multiples specified will show all applicable text values, i.e. fizzbuzz
 						value += combos[number];
 					}
 				}
