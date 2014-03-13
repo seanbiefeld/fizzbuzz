444f4de6c50d984661c5699db4958c212d2f94e5
Sean Biefeld <sbiefeld@gmail.com>
2014-03-12 19:59:19 -0500

    added remaing specs

--- HeadspringFizzBuzzLCE/FizzBuzzSpecs/FizzBuzzSpecs.cs
+++ HeadspringFizzBuzzLCE/FizzBuzzSpecs/FizzBuzzSpecs.cs
@@ -7,15 +7,14 @@ namespace FizzBuzzSpecs
     public class FizzBuzzContext : ContextSpecification
     {
 		protected FizzBuzz fizzBuzz = new FizzBuzz();
+		protected string output = "";
     }
 
 	[Concern("When finding fizz")]
 	public class when_finding_fizz_as_multiple_of_3 : FizzBuzzContext
 	{
-		private string output = "";
-
 		[Observation]
-		public void it_should_print_fizz_for_3()
+		public void it_should_return_fizz_for_3()
 		{
 			output = fizzBuzz.DoTheFizzBuzz(3, 3).First();
 
@@ -23,5 +22,39 @@ namespace FizzBuzzSpecs
 		}
 	}
 
-	
+	[Concern("When finding buzz")]
+	public class when_finding_buzz_as_multiple_of_5 : FizzBuzzContext
+	{
+		[Observation]
+		public void it_should_return_buzz_for_multiples_of_5()
+		{
+			output = fizzBuzz.DoTheFizzBuzz(5, 5).First();
+
+			output.ShouldEqual("buzz");
+		}
+	}
+
+	[Concern("When finding fizzbuzz")]
+	public class when_finding_fizzbuzz_as_multiple_of_3_and_5 : FizzBuzzContext
+	{
+		[Observation]
+		public void it_should_return_buzz_for_multiples_of_5()
+		{
+			output = fizzBuzz.DoTheFizzBuzz(15, 15).First();
+
+			output.ShouldEqual("fizzbuzz");
+		}
+	}
+
+	[Concern("When number is not a multiple of 3 or 5")]
+	public class when_number_is_not_a_multiple_of_3_or_5 : FizzBuzzContext
+	{
+		[Observation]
+		public void it_should_return_the_number()
+		{
+			output = fizzBuzz.DoTheFizzBuzz(2,2).First();
+
+			output.ShouldEqual("2");
+		}
+	}
 }
